70736ee3bb97b8f80f13140f38c6f10c97b7fe09
k1seul <briankan21@gmail.com>
2025-01-07 16:58:20 +0900

    added: parallel ddqn training script

---
+++ scripts/ddqn/ddqn.sh
@@ -0,0 +1,12 @@
+cd ../..
+
+python run_parallel.py \
+    --group_name ddqn \
+    --exp_name ddqn_baseline \
+    --config_name ddqn \
+    --seeds 0 1 \
+    --num_games 26 \
+    --num_devices 1 \
+    --num_exp_per_device 3 \
+    --games phoenix assault \
+
